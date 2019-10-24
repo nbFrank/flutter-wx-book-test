8fb79a54c1830397ac60e79607a05970d7e4d4c0
fangwei <19e_100005789728_fw168168@git.cloud.tencent.com>
2019-10-24 17:50:42 +0800

    1.修改测试

---
+++ lib/tab_navigator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
